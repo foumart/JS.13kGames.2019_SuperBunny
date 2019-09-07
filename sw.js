8b9e24fe86d87172a06fd725b470d9751bb015a1
Noncho Savov <inthewoodsbg@yahoo.com>
2019-09-07 19:42:29 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
-var version = 'v1'
-this.addEventListener('install', function(event){
+var ver = 'v1';
+self.addEventListener('install', event => {
 	event.waitUntil(
-		caches.open(version).then(cache => {
+		caches.open(ver).then(cache => {
 			return cache.addAll([
 				'index.html'
 			])
 		})
 	)
 })
-this.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
+	if(event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return
 	event.respondWith(
-		caches.match(event.request).then(function(resp) {
-			return resp || fetch(event.request).then(function(response) {
-				return caches.open(version).then(function(cache) {
+		caches.match(event.request).then(resp => {
+			return resp || fetch(event.request).then(response =>  {
+				return caches.open(ver).then(cache =>  {
 					cache.put(event.request, response.clone())
 					return response
 				})
